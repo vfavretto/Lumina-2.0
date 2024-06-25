da482faa8fdb78884d511557a4a728dcae426ab0
vfavretto <vf.favretto@gmail.com>
2024-06-25 00:58:21 -0300

    Footer

--- Lumina/src/app/app.component.ts
+++ Lumina/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./pages/header/header.component";
+import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'Lumina';
--- Lumina/src/app/pages/footer/footer.component.ts
+++ Lumina/src/app/pages/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
