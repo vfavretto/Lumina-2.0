e6a57a07dfbe012351c3d7a4eb4049257866e7aa
julinhaarte <juliasoares.contato@gmail.com>
2024-06-25 02:22:35 -0300

    sobre

--- Lumina/src/app/pages/about/about.component.ts
+++ Lumina/src/app/pages/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
